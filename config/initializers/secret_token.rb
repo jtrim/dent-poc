4bdbb32d64404a8c857fe41de6e47c6e85fb1fde
Jesse Trimble <jesseltrimble@gmail.com>
2013-01-26 20:47:34 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DentPoc::Application.config.secret_token = '0810d02f9bfdc477e7764938fb1395f55791c9a9f22b2ecb248568137d9392bc553f63450d395b0ea8515595629dab95c6080d7a1d449287b0afa7688c2f556c'
